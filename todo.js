00ddec845d7ee38d9a9fd1f29bf1ab9b2d886b4a
koma <213181439+k-k797@users.noreply.github.com>
2026-04-24 20:22:30 +0900

    ゴミ箱ボタンの位置変更

--- todo.js
+++ todo.js
@@ -1,6 +1,9 @@
 const input = document.querySelector('#todo-input');
 const btn = document.querySelector('#input-btn');
 const list = document.querySelector('#todo-list');
+const trashCanBtn = document.querySelector('#trashcan');
+const trashCanList = document.querySelector('#trashcan-list')
+let trashCanArray = [];
 
 const addTodo = () => {
     const todoText = input.value;
@@ -18,6 +21,7 @@ const addTodo = () => {
     checkbox.addEventListener('change', () => {
         if (checkbox.checked) {
             timeOutId = setTimeout(() => {
+                trashCanArray.push(todoText);
                 listItem.remove();
             }, 1000)
         } else {
@@ -34,4 +38,31 @@ input.addEventListener('keydown', (e) => {
     if (e.key === "Enter" && !e.isComposing) {
         addTodo();
     }
+})
+
+trashCanBtn.addEventListener('click', () => {
+    trashCanList.textContent = '';
+    console.log(trashCanArray);
+
+    for (let trashList of trashCanArray) {
+        const trashListItem = document.createElement('li');
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        const span = document.createElement('span');
+        span.textContent = trashList;
+        trashListItem.appendChild(checkbox);
+        trashListItem.appendChild(span);
+        trashCanList.appendChild(trashListItem);
+
+        checkbox.addEventListener('change', () => {
+            if (checkbox.checked) {
+                input.value = trashList;
+                addTodo();
+                trashListItem.remove();
+                trashCanArray = trashCanArray.filter(todo => todo !== trashList);
+            }
+        })
+
+    }
+
 })
